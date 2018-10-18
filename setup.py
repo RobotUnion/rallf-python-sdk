0afaf8130fd8f5e364a0731ecbdd790f36efe4c5
Lluis Santos <lluis@ngynx.com>
2018-10-18 19:51:57 +0200

    added Task

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="rallf",
-  version="0.0.4",
+  version="0.0.5",
   author="Lluis Santos",
   author_email="lluis@rallf.com",
   description="rallf.com Software Development Kit",
---
+++ src/Task.py
@@ -0,0 +1,14 @@
+
+
+class Task:
+  def __init__(self, robot):
+    self.robot = robot
+
+  def warmup(self):
+    pass
+
+  def run(self, input):
+    pass
+
+  def cooldown(self):
+    pass
