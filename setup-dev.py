57e59bbe3fb0a11452ace0ab488bd1f6fdb9d584
Lluis Santos <lluis@ngynx.com>
2019-01-15 21:29:22 +0100

    released version 0.3.2

--- setup-dev.py
+++ setup-dev.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
   name="rallf",
-  version="0.3.1-dev7",
+  version="0.3.2-dev8",
   author="Lluis Santos",
   author_email="lluis@rallf.com",
   license="MIT",
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
   name="rallf",
-  version="0.3.0",
+  version="0.3.2",
   author="Lluis Santos",
   author_email="lluis@rallf.com",
   license="MIT",
