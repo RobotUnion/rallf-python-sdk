bc92ea00354798d0acded17827aa458f35109b2b
Lluis Santos <lluis@ngynx.com>
2019-01-15 21:28:14 +0100

    fixes #9

--- rallf/sdk/listener.py
+++ rallf/sdk/listener.py
@@ -12,20 +12,27 @@ class Listener(Communicator):
     def listen(self, task):
         try:
             req = self.wait()
-            if 'method' in req:
-                try:
+            try:
+                if 'method' in req:
                     if req['method'] != "delegate":
                         raise RallfError(-32601, "Invalid method '%s'" % req['method'])
+                    try:
+                        task.network.event("routine:start", {"name": req['params']['routine'], "id": req["id"]})
 
-                    task.network.event("routine:start", {"name": req['params']['routine'], "id": req["id"]})
-                    resp = getattr(task, req['params']['routine'])(req['params']['args'])
-                    task.network.rpcresponse(id=req['id'], result=resp, error=False)
-                except RallfError as e:
-                    task.network.rpcresponse(id=req['id'], error=e.dict(), result=False)
-                finally:
-                    task.network.event("routine:end", {"name": req['params']['routine'], "id": req["id"]})
-            else:
-                self.mq.append(req)
+                        if req['params']['routine'] not in task.manifest['exports']:
+                            raise RallfError(-32601, "Method '%s' not exported in Task %s" % (req['params']['routine'], task.manifest['fqtn']))
+
+                        resp = getattr(task, req['params']['routine'])(req['params']['args'])
+                        task.network.rpcresponse(id=req['id'], result=resp, error=False)
+                    except RallfError as e:
+                        task.network.rpcresponse(id=req['id'], error=e.dict(), result=False)
+                    finally:
+                        task.network.event("routine:end", {"name": req['params']['routine'], "id": req["id"]})
+                else:
+                    self.mq.append(req)
+
+            except RallfError as e:
+                task.network.rpcresponse(id=req['id'], error=e.dict(), result=False)
 
         except JSONDecodeError:
             task.network.rpcresponse(id=None, error={"code": -32700, "message": "Parse error"})
